f1901923d523181af2fd41dc2c4a38f44abb83d6
kashaKatarzyna <cathylankie@gmail.com>
2016-06-08 11:20:06 +0300

    updated package.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,7 +11,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-mongoose.connect(process.env.MONGOLAB_YELLOW_URI || 'mongodb://localhost/dalliesandlallies');
+mongoose.connect(process.env.MONGOLAB_MAROON_URI || 'mongodb://localhost/dalliesandlallies');
 
 // app.use(express.static('public'));
 // app.use(express.static('node_modules'));
